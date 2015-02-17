e716ba02e6462e0fc539b5ae7b2c7d0c4a22e624
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2015-02-17 13:13:10 +0000

    Added initial setup.py file with PyPI configuration

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+This module defines the attributes of the
+PyPI package for the mbed SDK test suite ecosystem tools
+"""
+
+import os
+from distutils.core import setup
+from setuptools import find_packages
+
+DESCRIPTION = "Host tests module with extracted from mbed SDK 2.0 workspace_tools/host_tests. Decoupling for mbed 2.0 and 3.0 common use cases."
+OWNER_NAMES = 'Przemyslaw Wirkus'
+OWNER_EMAILS = 'Przemyslaw.Wirkus@arm.com'
+
+
+# Utility function to cat in a file (used for the README)
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='mbed-host-tests',
+      version='0.0.1',
+      description=DESCRIPTION,
+      long_description=read('README.md'),
+      author=OWNER_NAMES,
+      author_email=OWNER_EMAILS,
+      maintainer=OWNER_NAMES,
+      maintainer_email=OWNER_EMAILS,
+      url='https://github.com/mbedmicro/mbed',
+      packages=find_packages(),
+      license="Apache-2.0",
+      },
+      install_requires=[])
