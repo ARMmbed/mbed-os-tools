c5aa3b8d540997ea6f03d213ac808b628725846f
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2015-03-05 11:04:12 +0000

    Updated license files for Python source code

--- mbed_host_tests/__init__.py
+++ mbed_host_tests/__init__.py
@@ -1,6 +1,6 @@
 """
 mbed SDK
-Copyright (c) 2011-2013 ARM Limited
+Copyright (c) 2011-2015 ARM Limited
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,6 +13,8 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+Author: Przemyslaw Wirkus <Przemyslaw.Wirkus@arm.com>
 """
 
 from optparse import OptionParser
