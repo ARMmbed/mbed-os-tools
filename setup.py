1122dddaf7e10f844186cda877bae13f5c1f2fb3
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2015-05-26 11:49:48 +0100

    v0.1.10

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(fname):
 
 
 setup(name='mbed-ls',
-      version='0.1.9',
+      version='0.1.10',
       description=DESCRIPTION,
       long_description=read('README.md'),
       author=OWNER_NAMES,
