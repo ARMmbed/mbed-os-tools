2c4ba3de7515fe0fda119949825b40f3ec74f43d
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2015-06-18 10:44:29 +0100

    v0.0.8

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='mbed-greentea',
-      version='0.0.7',
+      version='0.0.8',
       description=DESCRIPTION,
       long_description=read('README.md'),
       author=OWNER_NAMES,
