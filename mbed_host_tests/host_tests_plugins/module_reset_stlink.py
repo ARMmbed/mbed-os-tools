93ed924c19a3c511829ac0be76065ef321779a88
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2015-07-31 11:01:52 +0100

    Added stlink plugins for copy and reset

---
+++ mbed_host_tests/host_tests_plugins/module_reset_stlink.py
@@ -0,0 +1,62 @@
+"""
+mbed SDK
+Copyright (c) 2011-2015 ARM Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Author: Przemyslaw Wirkus <Przemyslaw.Wirkus@arm.com>
+"""
+
+from host_test_plugins import HostTestPluginBase
+
+
+class HostTestPluginResetMethod_Stlink(HostTestPluginBase):
+
+    # Plugin interface
+    name = 'HostTestPluginResetMethod_Stlink'
+    type = 'ResetMethod'
+    stable = True
+    capabilities = ['stlink']
+    required_parameters = []
+
+    def setup(self, *args, **kwargs):
+        """ Configure plugin, this function should be called before plugin execute() method is used.
+        """
+        # Note you need to have eACommander.exe on your system path!
+        self.ST_LINK_CLI = 'ST-LINK_CLI.exe'
+        return True
+
+    def execute(self, capability, *args, **kwargs):
+        """! Executes capability by name
+
+        @param capability Capability name
+        @param args Additional arguments
+        @param kwargs Additional arguments
+
+        @details Each capability e.g. may directly just call some command line program or execute building pythonic function
+
+        @return Capability call return value
+        """
+        result = False
+        if self.check_parameters(capability, *args, **kwargs) is True:
+            if capability == 'stlink':
+                cmd = [self.ST_LINK_CLI,
+                       '-Rst']
+                result = self.run_command(cmd)
+        return result
+
+
+def load_plugin():
+    """ Returns plugin available in this module
+    """
+    return HostTestPluginResetMethod_Stlink()
