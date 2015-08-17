74bec9cba9f586eadf4e77e07cbf28e662f47339
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2015-08-17 16:15:02 +0100

    v0.1.13

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def read(fname):
 
 
 setup(name='mbed-host-tests',
-      version='0.1.12',
+      version='0.1.13',
       description=DESCRIPTION,
       long_description=read('README.md'),
       author=OWNER_NAMES,
