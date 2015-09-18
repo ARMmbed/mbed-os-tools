fb9f05efab9ad41eaed05e70e47850525cda969f
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2015-09-18 09:00:54 +0100

    Add unit tests to target info api

--- test/mbed_gt_target_info.py
+++ test/mbed_gt_target_info.py
@@ -22,7 +22,13 @@ from mbed_greentea import mbed_target_info
 class GreenteaTargetInfo(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.YOTTA_SEARCH_SSL_ISSUE = """/Library/Python/2.7/site-packages/requests/packages/urllib3/util/ssl_.py:90: InsecurePlatformWarning: A true SSLContext object is not available. This prevents urllib3 from configuring SSL appropriately and may cause certain SSL connections to fail. For more information, see https://urllib3.readthedocs.org/en/latest/security.html#insecureplatformwarning.
+  InsecurePlatformWarning
+frdm-k64f-gcc 0.0.24: Official mbed build target for the mbed frdm-k64f development board.
+frdm-k64f-armcc 0.0.16: Official mbed build target for the mbed frdm-k64f development board, using the armcc toolchain.
+/Library/Python/2.7/site-packages/requests/packages/urllib3/util/ssl_.py:90: InsecurePlatformWarning: A true SSLContext object is not available. This prevents urllib3 from configuring SSL appropriately and may cause certain SSL connections to fail. For more information, see https://urllib3.readthedocs.org/en/latest/security.html#insecureplatformwarning.
+  InsecurePlatformWarning
+"""
 
     def tearDown(self):
         pass
@@ -36,6 +42,16 @@ class GreenteaTargetInfo(unittest.TestCase):
         self.assertIn("frdm-k64f-armcc", mbed_target_info.parse_yotta_search_cmd_output("frdm-k64f-armcc 0.0.16: Official mbed build target for the mbed frdm-k64f development board, using the armcc toolchain."))
         self.assertEqual(None, mbed_target_info.parse_yotta_search_cmd_output(""))
         self.assertEqual(None, mbed_target_info.parse_yotta_search_cmd_output("additional results from https://yotta-private.herokuapp.com:"))
+
+    def test_parse_yotta_search_cmd_output_with_ssl_errors(self):
+        result = []
+        for line in self.YOTTA_SEARCH_SSL_ISSUE.splitlines():
+            yotta_target_name = mbed_target_info.parse_yotta_search_cmd_output(line)
+            if yotta_target_name:
+                result.append(yotta_target_name)
+        self.assertIn("frdm-k64f-gcc", result)
+        self.assertIn("frdm-k64f-armcc", result)
+        self.assertEqual(2, len(result))
 
 if __name__ == '__main__':
     unittest.main()
