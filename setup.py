0f3795a6e850da57967ebe2980796b6524e7a389
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2015-11-06 12:43:23 +0000

    v0.1.11
    * Experimental parallel execution (switch ```--parallel <int>```)

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='mbed-greentea',
-      version='0.1.10',
+      version='0.1.11',
       description=DESCRIPTION,
       long_description=read('README.md'),
       author=OWNER_NAMES,
