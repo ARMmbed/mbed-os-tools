6a56ef56e1b6d68799e3c04f38456c7e5b7556db
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2016-01-14 14:28:40 +0000

    Rename variable 'property' to 'prop'
    Aparently 'property' is a thing in Python

--- mbed_greentea/mbed_greentea_cli.py
+++ mbed_greentea/mbed_greentea_cli.py
@@ -431,9 +431,9 @@ def main_cli(opts, args, gt_instance_uuid=None):
         for mut in mbeds_list:
             if not all(mut.values()):
                 gt_logger.gt_log_err("can't detect all properties of the device!")
-                for property in mut:
-                    if not mut[property]:
-                        gt_logger.gt_log_tab("property '%s' is '%s'"% (property, str(mut[property])))
+                for prop in mut:
+                    if not mut[prop]:
+                        gt_logger.gt_log_tab("property '%s' is '%s'"% (prop, str(mut[prop])))
             else:
                 ready_mbed_devices.append(mut)
                 gt_logger.gt_log_tab("detected '%s' -> '%s', console at '%s', mounted at '%s', target id '%s'"% (
