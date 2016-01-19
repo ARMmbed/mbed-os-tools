d37dc009f1c4f6e8855657dd6dbf17df9332f765
Marius Lind Volstad <mlvolstad@NORLT0576.corp.atmel.com>
2016-01-19 17:16:42 +0100

    Add test for mbed parsing

--- test/os_win7.py
+++ test/os_win7.py
@@ -19,6 +19,12 @@ limitations under the License.
 import unittest
 from mbed_lstools.lstools_win7 import MbedLsToolsWin7
 
+# Since we don't have mock, let's monkey-patch
+
+def get_mbed_devices_new(self):
+    return [
+        ('\\DosDevices\\D:', '_??_USBSTOR#Disk&Ven_MBED&Prod_XPRO&Rev_1.00#9&35913356&0&ATML2127031800007973&0#{53f56307-b6bf-11d0-94f2-00a0c91efb8b}'),
+    ]
 
 class Win7TestCase(unittest.TestCase):
     """ Basic test cases checking trivial asserts
@@ -29,6 +35,24 @@ class Win7TestCase(unittest.TestCase):
 
     def test_os_supported(self):
         pass
+        
+    def test_get_mbeds(self):
+        
+        m = MbedLsToolsWin7()
+        
+        func_type = type(MbedLsToolsWin7.get_mbed_devices)
+        m.get_mbed_devices = func_type(get_mbed_devices_new, m, MbedLsToolsWin7)
+
+        mbeds = m.get_mbeds()
+        
+        self.assertIsNotNone(mbeds)
+        self.assertEqual(1, len(mbeds))
+        
+        mbed = mbeds[0]
+        
+        self.assertEqual("D:", mbed[0])
+        self.assertEqual("ATML2127031800007973", mbed[1])
+        
 
 
 if __name__ == '__main__':
