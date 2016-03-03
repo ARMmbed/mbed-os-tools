ee5eba340a9da7ab9edbea7485ec30139b4d4fe6
SteGut <stefan.gutmann@arm>
2016-03-03 14:44:09 +0000

    Modify condition to exit process

--- mbed_host_tests/host_tests_conn_proxy/conn_proxy.py
+++ mbed_host_tests/host_tests_conn_proxy/conn_proxy.py
@@ -180,7 +180,7 @@ def conn_process(event_queue, dut_event_queue, prn_lock, config):
             except QueueEmpty:
                 continue
             # Return if state machine in host_test_default has finished to end process
-            if key == '__host_test_finished':
+            if key == '__host_test_finished' and value == True:
                 connector.finish()
                 return 0
             connector.write_kv(key, value)
