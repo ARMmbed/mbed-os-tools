2dcfd286047afe968fcbf173daa287def34e06e9
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2016-04-06 13:15:54 +0100

    v0.2.5

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='mbed-ls',
-      version='0.2.4',
+      version='0.2.5',
       description=DESCRIPTION,
       long_description=read('README.md'),
       author=OWNER_NAMES,
