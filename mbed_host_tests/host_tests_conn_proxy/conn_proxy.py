411d1e01af03cfeec99f85438e9231384db72581
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2016-06-16 18:17:33 +0100

    Add ConectionPrimitive as base class for SerialConnectorPrimitive
    This is preparation for interexchangable CONN process

--- mbed_host_tests/host_tests_conn_proxy/conn_proxy.py
+++ mbed_host_tests/host_tests_conn_proxy/conn_proxy.py
@@ -58,15 +58,14 @@ class ConnectorPrimitive(object):
         raise NotImplementedError
 
 
-class SerialConnectorPrimitive(object):
-    def __init__(self, port, baudrate, prn_lock, config):
+class SerialConnectorPrimitive(ConnectorPrimitive):
+    def __init__(self, name, port, baudrate, prn_lock, config):
+        ConnectorPrimitive.__init__(self, name, prn_lock)
         self.port = port
         self.baudrate = int(baudrate)
         self.timeout = 0
         self.prn_lock = prn_lock
         self.config = config
-        self.logger = HtrunLogger(prn_lock, 'SERI')
-        self.LAST_ERROR = None
         self.target_id = self.config.get('target_id', None)
         self.serial_pooling = config.get('serial_pooling', 60)
         self.forced_reset_timeout = config.get('forced_reset_timeout', 1)
@@ -143,12 +142,6 @@ class SerialConnectorPrimitive(object):
             self.logger.prn_err(str(e))
         return payload
 
-    def write_kv(self, key, value):
-        kv_buff = "{{%s;%s}}\n"% (key, value)
-        self.write(kv_buff)
-        self.logger.prn_txd(kv_buff)
-        return kv_buff
-
     def flush(self):
         if self.serial:
             self.serial.flush()
@@ -203,7 +196,9 @@ def conn_process(event_queue, dut_event_queue, prn_lock, config):
     event_queue.put(('__timeout', serial_pooling, time()))
 
     logger.prn_inf("initializing serial port listener... ")
-    connector = SerialConnectorPrimitive(port,
+    connector = SerialConnectorPrimitive(
+        'SERI',
+        port,
         baudrate,
         prn_lock,
         config=config)
