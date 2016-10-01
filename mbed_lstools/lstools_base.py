f709f3ab6c709bf2296a9653cbddd44b43633342
andreas.larsson <andreas.larsson@connectblue.com>
2016-10-01 09:52:53 +0200

    Changed name for the target UBLOX_C029 to UBLOX_EVK_ODIN_W2

--- mbed_lstools/lstools_base.py
+++ mbed_lstools/lstools_base.py
@@ -132,7 +132,7 @@ class MbedLsToolsBase:
         "1168": "LPC11U68",
         "1234": "UBLOX_C027",
         "1235": "UBLOX_C027",
-        "1236": "UBLOX_C029",
+        "1236": "UBLOX_EVK_ODIN_W2",
         "1300": "NUC472-NUTINY",
         "1301": "NUMBED",
         "1549": "LPC1549",
