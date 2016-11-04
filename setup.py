a137586df4ded717812c443c600cbb7b6b69c4ed
Mohammad Azim Khan <mazimkhan@users.noreply.github.com>
2016-11-04 15:17:56 +0000

    Version 1.2.10
    Changes:
    - Fix issue were a method is attempted to be called on a NoneType.
    - Added new target Delta DFCM-NNN50

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='mbed-ls',
-      version='1.2.9',
+      version='1.2.10',
       description=DESCRIPTION,
       long_description=read('README.md'),
       author=OWNER_NAMES,
