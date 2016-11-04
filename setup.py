efc14fae98914adb9aaeb8705c9c9d56f36e59ca
Mohammad Azim Khan <mazimkhan@users.noreply.github.com>
2016-11-04 17:03:58 +0000

    Release 1.1.5
    Fix:
    - Catch serial error and exit

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='mbed-host-tests',
-      version='1.1.4',
+      version='1.1.5',
       description=DESCRIPTION,
       long_description=read('README.md'),
       author=OWNER_NAMES,
