5633fca55a3c130f04bf0857ea8f84ce3d1d6e61
Anna Bridge <Anna.Bridge@arm.com>
2017-05-16 16:42:04 +0100

    Version 1.2.13

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='mbed-ls',
-      version='1.2.12',
+      version='1.2.13',
       description=DESCRIPTION,
       long_description=read('README.md'),
       author=OWNER_NAMES,
