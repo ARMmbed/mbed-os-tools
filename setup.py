8928311d3b370558c9fcb1499a57202c87c12772
Mohammad Azim Khan <mazimkhan@users.noreply.github.com>
2017-07-10 16:37:22 +0100

    Release 1.2.14
    Fixes and enhancements:
    - Add NRF52840 target to the list of platforms.
    - Change 5501 target name to GR_LYCHEE
    - shenmengkai + shenmengkai	New platform MTM_MTCONNECT04S
    - add decode to mount to support flasher
    - Fix unhandled exception for platforms without valid ID on OSX
    - Added UBLOX_EVK_NINA_B1 target id i.e. 1237
    - Add support for NUCLEO L496ZG
    - Support for ARM_MPS2 detection

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='mbed-ls',
-      version='1.2.13',
+      version='1.2.14',
       description=DESCRIPTION,
       long_description=read('README.md'),
       author=OWNER_NAMES,
