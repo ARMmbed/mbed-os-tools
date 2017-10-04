70e85078329fe2aaa12f2b3300caa0640604d4aa
Jimmy Brisson <theotherjimmy@gmail.com>
2017-10-04 14:03:47 -0500

    Move individual command logic into functions

--- mbed_lstools/main.py
+++ mbed_lstools/main.py
@@ -82,44 +82,115 @@ def mbed_lstools_os_info():
     return result
 
 
+def get_version():
+    """! Get mbed-ls Python module version string """
+    import pkg_resources  # part of setuptools
+    return pkg_resources.require("mbed-ls")[0].version
+
+def print_version(mbeds, args):
+    print(get_version())
+
+def print_mbeds(mbeds, args, simple):
+    devices = mbeds.list_mbeds(unique_names=True, read_details_txt=True)
+    if devices:
+        from prettytable import PrettyTable
+        columns = ['platform_name', 'platform_name_unique', 'mount_point',
+                    'serial_port', 'target_id', 'daplink_version']
+        pt = PrettyTable(columns)
+        for d in devices:
+            pt.add_row([d.get(col, None) or 'unknown' for col in columns])
+        print(pt.get_string(border=not simple, header=not simple,
+                            padding_width=1, sortby='platform_name_unique'))
+
+def print_table(mbeds, args):
+    return print_mbeds(mbeds, args, False)
+
+def print_simple(mbeds, args):
+    return print_mbeds(mbeds, args, True)
+
+def mock_platform(mbeds, args):
+    for token in args.mock_platform.split(','):
+        if ':' in token:
+            oper = '+' # Default
+            mid, platform_name = token.split(':')
+            if mid and mid[0] in ['+', '-']:
+                oper = mid[0]   # Operation (character)
+                mid = mid[1:]   # We remove operation character
+            mbeds.mock_manufacture_id(mid, platform_name, oper=oper)
+        elif token and token[0] in ['-', '!']:
+            # Operations where do not specify data after colon: --mock=-1234,-7678
+            oper = token[0]
+            mid = token[1:]
+            mbeds.mock_manufacture_id(mid, 'dummy', oper=oper)
+        else:
+            logger.error("Could not parse mock from token: '%s'", token)
+
+def list_platforms(mbeds, args):
+    print(mbeds.list_manufacture_ids())
+
+def mbeds_as_json(mbeds, args):
+    print(json.dumps(mbeds.list_mbeds(unique_names=True,
+                                      read_details_txt=True),
+                     indent=4, sort_keys=True))
+
+def json_by_target_id(mbeds, args):
+    print(json.dumps({m['target_id']: m for m
+                      in mbeds.list_mbeds(unique_names=True,
+                                          read_details_txt=True)},
+                     indent=4, sort_keys=True))
+
+def json_platforms(mbeds, args):
+    platforms = set()
+    for d in mbeds.list_mbeds():
+        platforms |= set([d['platform_name']])
+    print(json.dumps(list(platforms), indent=4, sort_keys=True))
+
+def json_platforms_ext(mbeds, args):
+    platforms = defaultdict(lambda: 0)
+    for d in mbeds.list_mbeds():
+        platforms[d['platform_name']] += 1
+    print(json.dumps(platforms, indent=4, sort_keys=True))
+
 def parse_cli():
-    """! Configure CLI (Command Line OPtions) options
+    """! Parse the command line
 
-    @return Returns OptionParser's tuple of (options, arguments)
-
-    @details Add new command line options here to control 'mbedls' command line iterface
+    @return Retrun a namespace that contains:
+     * command - python function to run
+     * skip_retarget - bool indicting to skip retargeting
+     * list_unmounted - list boards that are not mounted
+     * debug - turn on debug logging
     """
     parser = argparse.ArgumentParser()
+    parser.set_defaults(command=print_table)
 
     commands = parser.add_mutually_exclusive_group()
     commands.add_argument(
-        '-s', '--simple', dest='simple', default=False, action="store_true",
-        help='Parser friendly verbose mode')
+        '-s', '--simple', dest='command', action='store_const',
+        const=print_simple, help='Parser friendly verbose mode')
     commands.add_argument(
-        '-j', '--json', dest='json', default=False, action="store_true",
+        '-j', '--json', dest='command', action='store_const',
+        const=mbeds_as_json,
         help='JSON formatted list of targets detailed information')
     commands.add_argument(
-        '-J', '--json-by-target-id', dest='json_by_target_id', default=False,
-        action="store_true",
+        '-J', '--json-by-target-id', dest='command', action='store_const',
+        const=json_by_target_id,
         help='JSON formatted dictionary ordered by TargetID of targets detailed information')
     commands.add_argument(
-        '-p', '--json-platforms', dest='json_platforms', default=False,
-        action="store_true",
+        '-p', '--json-platforms', dest='command', action='store_const',
+        const=json_platforms,
         help='JSON formatted list of available platforms')
     commands.add_argument(
-        '-P', '--json-platforms-ext', dest='json_platforms_ext', default=False,
-        action="store_true",
+        '-P', '--json-platforms-ext', dest='command', action='store_const',
+        const=json_platforms_ext,
         help='JSON formatted dictionary of platforms count')
     commands.add_argument(
-        '-l', '--list', dest='list_platforms', default=False,
-        action="store_true",
+        '-l', '--list', dest='command', action='store_const', const=list_platforms,
         help='List all platforms and corresponding TargetID values mapped by mbed-ls')
     commands.add_argument(
-        '--version', dest='version', default=False,
-        action="store_true",
+        '--version', dest='command', action='store_const', const=print_version,
         help='Prints package version and exits')
     commands.add_argument(
-        '-m', '--mock', dest='mock_platform',
+        '-m', '--mock', dest='command', action='store_const', const=mock_platform,
         help='Add locally manufacturers id and platform name. Example --mock=12B4:NEW_PLATFORM')
 
     parser.add_argument(
@@ -136,19 +207,10 @@ def parse_cli():
 
     return parser.parse_args()
 
-def get_mbedls_version():
-    """! Get mbed-ls Python module version string """
-    import pkg_resources  # part of setuptools
-    version = pkg_resources.require("mbed-ls")[0].version
-    return version
-
 def mbedls_main():
     """! Function used to drive CLI (command line interface) application
     @return Function exits with success code
-    @details Function exits back to command line with ERRORLEVEL
     """
-    args = parse_cli()
-
     root_logger = logging.getLogger("")
     try:
         import colorlog
@@ -156,85 +218,28 @@ def mbedls_main():
             format='%(log_color)s%(levelname)s%(reset)s:%(name)s:%(message)s')
     except ImportError:
         logging.basicConfig()
+
+    args = parse_cli()
+
     if args.debug:
         root_logger.setLevel(logging.DEBUG)
     else:
         root_logger.setLevel(logging.INFO)
-    logger.debug("mbed-ls ver. %s", get_mbedls_version())
-    logger.debug("host: %s",  str((mbed_lstools_os_info())))
+    logger.debug("mbed-ls ver. %s", get_version())
+    logger.debug("host: %s",  str(mbed_lstools_os_info()))
 
     mbeds = create(skip_retarget=args.skip_retarget,
                    list_unmounted=args.list_unmounted,
-                   force_mock=args.mock_platform)
+                   force_mock=args.command is mock_platform)
 
     if mbeds is None:
         logger.critical('This platform is not supported! Pull requests welcome at github.com/ARMmbed/mbed-ls')
         sys.exit(-1)
 
+    ret_code = args.command(mbeds, args)
+    if not ret_code:
+        ret_code = 0
 
-    if args.list_platforms:
-        print(mbeds.list_manufacture_ids())
-        sys.exit(0)
+    logger.debug("Return code: %d", ret_code)
 
-    if args.mock_platform:
-        if args.mock_platform == '*':
-            if args.json:
-                print(json.dumps(mbeds.mock_read(), indent=4))
-
-        for token in args.mock_platform.split(','):
-            if ':' in token:
-                oper = '+' # Default
-                mid, platform_name = token.split(':')
-                if mid and mid[0] in ['+', '-']:
-                    oper = mid[0]   # Operation (character)
-                    mid = mid[1:]   # We remove operation character
-                mbeds.mock_manufacture_id(mid, platform_name, oper=oper)
-            elif token and token[0] in ['-', '!']:
-                # Operations where do not specify data after colon: --mock=-1234,-7678
-                oper = token[0]
-                mid = token[1:]
-                mbeds.mock_manufacture_id(mid, 'dummy', oper=oper)
-            else:
-                logger.error("Could not parse mock from token: '%s'", token)
-
-    elif args.json:
-        print(json.dumps(mbeds.list_mbeds(unique_names=True,
-                                          read_details_txt=True),
-                         indent=4, sort_keys=True))
-
-    elif args.json_by_target_id:
-        print(json.dumps({m['target_id']: m for m
-                          in mbeds.list_mbeds(unique_names=True,
-                                              read_details_txt=True)},
-                         indent=4, sort_keys=True))
-
-    elif args.json_platforms:
-        platforms = set()
-        for d in mbeds.list_mbeds():
-            platforms |= set([d['platform_name']])
-        print(json.dumps(list(platforms), indent=4, sort_keys=True))
-
-    elif args.json_platforms_ext:
-        platforms = defaultdict(lambda: 0)
-        for d in mbeds.list_mbeds():
-            platforms[d['platform_name']] += 1
-        print(json.dumps(platforms, indent=4, sort_keys=True))
-
-    elif args.version:
-        print(get_mbedls_version())
-
-    else:
-        devices = mbeds.list_mbeds(unique_names=True, read_details_txt=True)
-        if devices:
-            from prettytable import PrettyTable
-            columns = ['platform_name', 'platform_name_unique', 'mount_point',
-                       'serial_port', 'target_id', 'daplink_version']
-            pt = PrettyTable(columns)
-            for d in devices:
-                pt.add_row([d.get(col, None) or 'unknown' for col in columns])
-            print(pt.get_string(border=True, header=True, padding_width=1,
-                                sortby='platform_name_unique'))
-
-    logger.debug("Return code: %d", mbeds.ERRORLEVEL_FLAG)
-
-    sys.exit(mbeds.ERRORLEVEL_FLAG)
+    sys.exit(ret_code)
