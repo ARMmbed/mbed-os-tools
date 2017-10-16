9eecc270ffe36896b3a37c8e9e4f2d13592ed71c
Jimmy Brisson <theotherjimmy@gmail.com>
2017-10-16 13:45:53 -0500

    Release version 1.3.3
    Fixes:
     - Startup without colorlog works
     - Test that startup works without colorlog in CI

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname), encoding="utf8").read()
 
 setup(name='mbed-ls',
-      version='1.3.2',
+      version='1.3.3',
       description=DESCRIPTION,
       long_description=read('README.md'),
       author=OWNER_NAMES,
