83b950238f328523519463465b1c70959e2e74c0
Jimmy Brisson <theotherjimmy@gmail.com>
2017-11-02 11:20:09 -0500

    Release version 1.3.2
    Fixes:
     * Correct target property priority
     * Remove trace back from parsing invalid keywords

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname), encoding="utf-8").read()
 
 setup(name='mbed-greentea',
-      version='1.3.1',          # 
+      version='1.3.2',
       description=DESCRIPTION,
       long_description=read('README.md'),
       author=OWNER_NAMES,
