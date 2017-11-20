geky/htrun

Allow a host test to reset the device under test
Add a host test reset function to allow the current DUT to be reset.
This allow unexpected power loss to be tested.

mbed_host_tests/host_tests_conn_proxy/conn_proxy.py
@@ -231,10 +231,15 @@ def conn_process(event_queue, dut_event_queue, config):
         else:
             # Return if state machine in host_test_default has finished to end process
             if key == '__host_test_finished' and value == True:
-                logger.prn_inf("received special even '%s' value='%s', finishing"% (key, value))
+                logger.prn_inf("received special event '%s' value='%s', finishing"% (key, value))
                 connector.finish()
                 return 0
-            if not connector.write_kv(key, value):
+            elif key == '__reset_dut':
+                logger.prn_inf("received special event '%s', resetting dut" % (key))
+                connector.reset()
+                event_queue.put(("soft_reset_dut_complete", 0, time()))
+            elif not connector.write_kv(key, value):
+                connector.write_kv(key, value)
                 __notify_conn_lost()
                 break
 

mbed_host_tests/host_tests_runner/host_test_default.py
@@ -366,6 +366,9 @@ class DefaultTestSelector(DefaultTestSelectorBase):
                         self.logger.prn_inf("%s(%s)" % (key, str(value)))
                         result = value
                         event_queue.put(('__exit_event_queue', 0, time()))
+                    elif key == '__soft_reset_dut':
+                        # This event only resets the dut, not the host test
+                        dut_event_queue.put(('__reset_dut', True, time()))
                     elif key == '__reset_dut':
                         # Disconnect to avoid connection lost event
                         dut_event_queue.put(('__host_test_finished', True, time()))

ARMmbed/mbed-os-tools

mbed_host_tests/host_tests_conn_proxy/conn_proxy.py
@@ -231,10 +231,15 @@ def conn_process(event_queue, dut_event_queue, config):
         else:
             # Return if state machine in host_test_default has finished to end process
             if key == '__host_test_finished' and value == True:
-                logger.prn_inf("received special even '%s' value='%s', finishing"% (key, value))
+                logger.prn_inf("received special event '%s' value='%s', finishing"% (key, value))
                 connector.finish()
                 return 0
-            if not connector.write_kv(key, value):
+            elif key == '__reset':
+                logger.prn_inf("received special event '%s', resetting dut" % (key))
+                connector.reset()
+                event_queue.put(("reset_complete", 0, time()))
+            elif not connector.write_kv(key, value):
+                connector.write_kv(key, value)
                 __notify_conn_lost()
                 break
 

mbed_host_tests/host_tests_runner/host_test_default.py
@@ -366,6 +366,9 @@ class DefaultTestSelector(DefaultTestSelectorBase):
                         self.logger.prn_inf("%s(%s)" % (key, str(value)))
                         result = value
                         event_queue.put(('__exit_event_queue', 0, time()))
+                    elif key == '__reset':
+                        # This event only resets the dut, not the host test
+                        dut_event_queue.put(('__reset', True, time()))
                     elif key == '__reset_dut':
                         # Disconnect to avoid connection lost event
                         dut_event_queue.put(('__host_test_finished', True, time()))