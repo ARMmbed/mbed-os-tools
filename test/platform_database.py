5d247c75ae8a0f45ff9e208695dffe23c3b9cefd
Brian Daniels <brianddaniels@gmail.com>
2018-11-07 10:26:23 -0600

    Revert "Fix test_broken_database"
    This reverts commit 10182a1995be810175a6af0fe8875f9f26458ede.
    
    The way this test was fixed was incorrect.

--- test/platform_database.py
+++ test/platform_database.py
@@ -70,16 +70,9 @@ class EmptyPlatformDatabaseTests(unittest.TestCase):
     def test_broken_database(self):
         """Verify that the platform database correctly reset's its database
         """
-        stringio = MagicMock()
-        def mock_open(*args, **kwargs):
-            arg = args[1] if len(args) > 1 else kwargs.get('mode', 'r')
-            if 'r' in arg:
-                raise IOError("Bogus")
-            else:
-                return stringio
-
         with patch("mbed_tools.detect.platform_database.open") as _open:
-            _open.side_effect = mock_open
+            stringio = MagicMock()
+            _open.side_effect = (IOError("Bogus"), stringio)
             self.pdb = PlatformDatabase([LOCAL_PLATFORM_DATABASE])
             stringio.__enter__.return_value.write.assert_called_with(
                 unicode(json.dumps(DEFAULT_PLATFORM_DB)))
