5173db5cbcb0df03d262ea81432f37dc0c369e17
Brian Daniels <brianddaniels@gmail.com>
2018-12-11 14:20:34 -0600

    Updating all copyrights to include 2018

--- legacy/mbed-ls/mbed_lstools/main.py
+++ legacy/mbed-ls/mbed_lstools/main.py
@@ -2,7 +2,7 @@
 
 """
 mbed SDK
-Copyright (c) 2011-2015 ARM Limited
+Copyright (c) 2011-2018 ARM Limited
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
