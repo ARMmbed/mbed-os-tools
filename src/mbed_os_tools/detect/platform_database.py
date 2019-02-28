e075b734c688272d30eb1e95d839f840bf11673a
Ashok Rao <Ashok.Rao@arm.com>
2019-02-28 12:23:02 +0000

    Add STM_L475 MTB

--- src/mbed_os_tools/detect/platform_database.py
+++ src/mbed_os_tools/detect/platform_database.py
@@ -104,6 +104,7 @@ DEFAULT_PLATFORM_DB = {
         u"0462": u"MTB_USI_WM_BN_BM_22",
         u"0465": u"MTB_LAIRD_BL654",
         u"0466": u"MTB_MURATA_WSM_BL241",
+        u"0468": u"MTB_STM_L475",
         u"0472": u"MTB_ACONNO_ACN52832",
         u"0500": u"SPANSION_PLACEHOLDER",
         u"0505": u"SPANSION_PLACEHOLDER",
