a09a6d518d66fd1c982ddacbbd2739ed1b05a8ee
Devaraj Ranganna <devaraj.ranganna@arm.com>
2020-10-12 17:03:45 +0100

    ci: add support for python 3.8

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python',
         'Topic :: Software Development :: Build Tools',
         'Topic :: Software Development :: Embedded Systems',
